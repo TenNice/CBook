2c267d8c51d4b487fcfd1fc2f9ddc4acd553af82
kosihoon <kosihoon1@gmail.com>
2022-12-08 13:00:08 +0900

    클래스 생성, 멤버 함수 정의, 생성자를 통한 멤버 변수 초기화

---
+++ ch1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+class OrangeBox {
+public:
+	// 생성자 선언
+	OrangeBox();
+	void Add(int addorangeBox);
+	void Del(int delorangeBox);
+	void Empty();
+	// 인라인 함수
+	int GetTotal() {
+		return total;
+	};
+private:
+	int total;
+};
+
+// 생성자 정의
+OrangeBox::OrangeBox()
+{
+	// 멤버 변수 초기화
+	total = 0;
+}
+
+void OrangeBox::Add(int addorange)
+{
+	total += addorange;
+}
+
+void OrangeBox::Empty()
+{
+	total = 0;
+}
+
+void OrangeBox::Del(int delorange)
+{
+	total -= delorange;
+	if (total < 0)
+		Empty();
+}
+
+int main()
+{
+	OrangeBox myOrangeBox;
+	myOrangeBox.Del(1);
+	myOrangeBox.Add(1);
+
+	int num = myOrangeBox.GetTotal();
+
+	printf("상자 속의 오렌지 : %d개\n", myOrangeBox.GetTotal());
+
+	return 0;
+}
